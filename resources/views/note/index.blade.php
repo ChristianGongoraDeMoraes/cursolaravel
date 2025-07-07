c8915ff1cb928b1075d1be2c134778ff31d6ce5c
chris <christianpimpo304@hotmail.com>
2025-07-07 18:56:49 -0300

    .

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
-    <h1>index</h1>
+    <div class="note-container">
+        <a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                </div>
+                <div class="note-buttons">
+                    <a href="{{ route('note.view', $note) }}" class="note-edit=button">View</a>
+                    <a href="{{ route('note.edit', $note) }}" class="note-edit=button">Edit</a>
+                    <button class="note-delete-button">Delete</button>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
